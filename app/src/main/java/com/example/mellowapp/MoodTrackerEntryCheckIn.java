78a8f512d0b2251ec7407988c68996d828641440
ChandrasenaAA_IT18134254 <IT18134254@my.sliit.lk>
2021-08-24 20:30:04 +0530

    new commit

---
+++ app/src/main/java/com/example/mellowapp/MoodTrackerEntryCheckIn.java
@@ -0,0 +1,14 @@
+package com.example.mellowapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MoodTrackerEntryCheckIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mood_tracker_entry_check_in);
+    }
+}
