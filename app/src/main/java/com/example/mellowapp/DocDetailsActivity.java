d58d43a45da09e7e3efdd0c68cf1c92da9276d0d
visnaOshani <jayasinghe548@gmail.com>
2021-08-24 12:52:50 +0530

    doc details activity edited

---
+++ app/src/main/java/com/example/mellowapp/DocDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.mellowapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DocDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doc_details);
+    }
+}
