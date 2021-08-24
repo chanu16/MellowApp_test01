97b7f2f58f104e6bf29598014b8194aa4eb59d76
ChandrasenaAA_IT18134254 <IT18134254@my.sliit.lk>
2021-08-24 18:22:36 +0530

    second commit aadi

---
+++ app/src/main/java/com/example/mellowapp/JournalEntryCheck.java
@@ -0,0 +1,14 @@
+package com.example.mellowapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JournalEntryCheck extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_journal_entry_check);
+    }
+}
---
+++ app/src/main/java/com/example/mellowapp/MoodTrackerScreen.java
@@ -0,0 +1,14 @@
+package com.example.mellowapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MoodTrackerScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mood_tracker_screen);
+    }
+}
